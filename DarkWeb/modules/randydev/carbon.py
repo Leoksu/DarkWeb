c726ff40b2866d353781c68c8893de9e8f897395
xtsea <90479255+xtsea@users.noreply.github.com>
2023-01-18 21:15:38 +0700

    Update carbon.py

--- DarkWeb/modules/randydev/carbon.py
+++ DarkWeb/modules/randydev/carbon.py
@@ -13,10 +13,15 @@ from DarkWeb.helper.misc import *
 from pykillerx import *
 from pykillerx.helper.basic import *
 from pykillerx.helper.hacking import *
-from pykillerx.helper.tools import*
 from pykillerx.helper import *
 from pykillerx.help import *
 
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
+    return image
 
 @ren.on_message(filters.command("carbon", cmd) & filters.me)
 async def carbon_func(client: Client, message: Message):
