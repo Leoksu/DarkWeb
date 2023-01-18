63c0d9c071ddd96c8a74874cf231ba4a782e16bf
xtsea <90479255+xtsea@users.noreply.github.com>
2023-01-18 15:03:51 +0700

    Create cmd.py

---
+++ DarkWeb/helper/cmd.py
@@ -0,0 +1,11 @@
+# Create Modules By @xtsea
+from pyrogram import *
+from pyrogram import Client as ren
+import os 
+from os import getenv
+
+cmd = [".", "+", "-"]
+
+command = filters.command
+
+dont_know = -1001011938077
