b16c174e0fa132c544287d6c0865dbaf989813d0
xtsea <90479255+xtsea@users.noreply.github.com>
2023-01-22 20:11:47 +0700

    Create sysinfo.py

---
+++ DarkWeb/modules/randydev/sysinfo.py
@@ -0,0 +1,26 @@
+# create by @xtsea
+
+from io import BytesIO
+from pyrogram import Client as ren
+from pyrogram import *
+from pyrogram.types import *
+
+from DarkWeb.modules.randydev.carbon import make_carbon
+from DarkWeb.modules.randydev.dev import shell_exec
+
+from pykillerx import *
+
+@ren.on_message(filters.command("neofetch", cmd) & filters.me)
+async def neofetch(client, Client, message: Message):
+    noob = await message.reply_text("`Prossing.....`")
+    try:
+        neofetch = (await shell_exec("neofetch --stdout"))[0]
+        carbon = await make_carbon(neofetch)
+        await client.send_photo(message.chat.id, carbon)
+        await noob.delete()
+    except BaseException:
+        pass
+
+# you can add modules
+
+# this code
